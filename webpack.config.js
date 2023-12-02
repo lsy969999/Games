6114cd4f2b10583d081d47e456fdce1cfd9dcb3f
lsy969999 <lsy969999@gmail.com>
2023-12-02 18:53:30 +0900

    404 test

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
