4cf270b1c707e1f82b07317b4fe06dd2850c478d
lsy969999 <lsy969999@gmail.com>
2023-12-03 17:59:28 +0900

    test

--- src/pages/lsy-test/index.ts
+++ src/pages/lsy-test/index.ts
@@ -9,14 +9,61 @@ const app = new PIXI.Application<HTMLCanvasElement>({
 
 document.body.appendChild(app.view);
 
-const bunny = PIXI.Sprite.from('https://pixijs.com/assets/bunny.png')
+const basicText = new PIXI.Text('Basic text in pixi1');
+basicText.x = 50;
+// basicText.y = 100;
+app.stage.addChild(basicText);
 
-bunny.anchor.set(0.5)
-bunny.x = app.screen.width / 2
-bunny.y = app.screen.height / 2
+const basicText2 = new PIXI.Text('Basic text in pixi2');
+basicText2.x = 50;
+basicText2.y = 100;
+app.stage.addChild(basicText2);
 
-app.stage.addChild(bunny)
+const texture = PIXI.Texture.from('https://pixijs.com/assets/bunny.png');
+texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
-app.ticker.add((delta)=>{
-  bunny.rotation += 0.0001 * delta
-})
+for (let i = 0; i < 10; i++) {
+    createBunny(
+        Math.floor(Math.random() * app.screen.width),
+        Math.floor(Math.random() * app.screen.height),
+    );
+}
+
+function createBunny(x: number, y: number) {
+    const bunny = new PIXI.Sprite(texture);
+    bunny.interactive = true;
+    // bunny.buttonMode = true;
+    bunny.anchor.set(0.5);
+    bunny.scale.set(3);
+    bunny.on('pointerdown', onDragStart);
+    bunny.x = x;
+    bunny.y = y;
+    app.stage.addChild(bunny);
+}
+
+let dragTarget: PIXI.Sprite | null = null;
+
+app.stage.interactive = true;
+app.stage.hitArea = new PIXI.Rectangle(0, 0, app.screen.width, app.screen.height);
+app.stage.on('pointerup', onDragEnd);
+app.stage.on('pointerupoutside', onDragEnd);
+
+function onDragMove(event: any) {
+    if (dragTarget) {
+        dragTarget.parent?.toLocal(event.data.global, undefined, dragTarget.position);
+    }
+}
+
+function onDragStart(this: PIXI.Sprite, event: any) {
+  this.alpha = 0.5;
+  dragTarget = this;
+  app.stage.on('pointermove', onDragMove);
+}
+
+function onDragEnd() {
+    if (dragTarget) {
+        app.stage.off('pointermove', onDragMove);
+        dragTarget.alpha = 1;
+        dragTarget = null;
+    }
+}
